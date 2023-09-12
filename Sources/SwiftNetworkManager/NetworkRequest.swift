f2fabf7df7efb7dec674eeca2cf49a7559c78a3a
John Baker <baker2795@gmail.com>
2023-09-12 19:21:52 -0400

    add public init

--- Sources/SwiftNetworkManager/NetworkRequest.swift
+++ Sources/SwiftNetworkManager/NetworkRequest.swift
@@ -11,6 +11,13 @@ public struct NetworkRequest<ResponseType: NetworkResponse> {
     public let url: URL
     public var headers: [String: String]?
     public var body: Data?
+    
+    public init(method: NetworkRequest<ResponseType>.HTTPMethod, url: URL, headers: [String : String]? = nil, body: Data? = nil) {
+        self.method = method
+        self.url = url
+        self.headers = headers
+        self.body = body
+    }
 }
 
 public extension NetworkRequest {
